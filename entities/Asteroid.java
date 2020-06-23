c70137c8e7d7c969b6c77b0b71d30f55d12f9bc5
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-23 23:08:50 +0300

    Create Asteroid.java

---
+++ entities/Asteroid.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Asteroid extends HeavenlyBody {
+
+    public Asteroid(String name, double orbitalPeriod, BodyTypes bodyType) {
+        super(name, orbitalPeriod, bodyType);
+    }
+}
