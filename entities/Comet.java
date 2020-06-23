17cfd938585680ab7f4e944a7f3b44210f7189b9
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-23 23:09:08 +0300

    Create Comet.java

---
+++ entities/Comet.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Comet extends HeavenlyBody {
+
+    public Comet(String name, double orbitalPeriod, BodyTypes bodyType) {
+        super(name, orbitalPeriod, bodyType);
+    }
+}
