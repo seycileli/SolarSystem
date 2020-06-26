8a22e9a25b061686d712f03fcb27e4f57a7737eb
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-27 00:03:20 +0300

    Create Asteroids.java

---
+++ solarSystem/Asteroids.java
@@ -0,0 +1,82 @@
+package solarSystem;
+
+public class Asteroids {
+
+    public static void generalInformation() {
+        System.out.println("\n------------------------------------------" +
+                "\n \t General Information about Asteroids " +
+                "\n------------------------------------------" +
+                "\n | Asteroids are small, rocky solar system bodies that populate interplanetary space out to " +
+                "\n | the orbit of Jupiter. There are millions of them, and they are often grouped by their " +
+                "\n | composition. " +
+                "\n | " +
+                "\n | The planetary science community refers to them as minor planets, a general term applied " +
+                "\n | to solar system bodies smaller than moons. Asteroids are mainly made of materials left " +
+                "\n | over from the formation of the inner solar system words. " +
+                "\n | Most of them orbit the Sun between Mars and Jupiter, although there are groups of " +
+                "\n | them that orbit closer. " +
+                "\n | " +
+                "\n | Asteroids come in three composition classes. C-types (chondrites) " +
+                "\n | are made of clay and silicate rocks. S-types are the so-called “stony” asteroids and " +
+                "\n | are made mostly of silicate rocks and nickel-iron mixtures. " +
+                "\n | M-types are metallic nickel-iron. " +
+                "\n | These categories indicate how far from the Sun they formed in the early solar system.");
+
+        System.out.println(
+                "\n ~ Facts about Asteroids " +
+                        "\n \t (1) Asteroids are clues to the formation of the rocky planets of our solar system. " +
+                        "\n            The objects we see today are leftover from a time when the solar system " +
+                        "\n            formed 4.5 billion years ago. There are a LOT of these leftovers out there. " +
+                        "\n            Let’s learn some cool facts about them." +
+                        "\n \t (2) Asteroids aren’t the only things that hit Earth. Each day, more than 100 tons of " +
+                        "\n            material from asteroids and comets falls toward Earth. " +
+                        "\n            Most of it is destroyed by friction as it passes through our atmosphere. " +
+                        "\n            If something DOES hit the ground, it is known as a meteorite." +
+                        "\n \t (3) While asteroid impacts were more common in the past, they aren’t as frequent today." +
+                        "\n \t (4) An asteroid impact some 65 million years ago contributed to the extinction of the " +
+                        "\n            dinosaurs. " +
+                        "\n            (It was one of several factors that affected all life on Earth at that time.)" +
+                        "\n \t (5) A car-sized meteoroid (a piece of asteroid) falls into Earth’s atmosphere about " +
+                        "\n            once a year. The result is a beautiful fireball, " +
+                        "\n            but the meteoroid usually burns up before reaching the ground." +
+                        "\n \t (6) Asteroids are rich in precious metals and other metals, as well as water." +
+                        "\n \t (7) Some asteroids are actually blown-out comets. The ices are gone, " +
+                        "\n            and all that’s left is the rocky material." +
+                        "\n \t (8) Some asteroids have moons of their own!" +
+                        "\n \t (9) Most asteroids orbit the Sun in the Asteroid Belt, which lies " +
+                        "\n            between Mars and Jupiter." +
+                        "\n \t (10) Asteroids are also referred to as minor planets or planetoids. \n");
+
+        System.out.println("---------------------------------------" +
+                "\n     Near Earth Asteroids & Impacts " +
+                "\n---------------------------------------" +
+                "\n | Earth and the other planets formed as objects in the early system smacked together and made " +
+                "\n | larger worlds. The process of collision and impact continues to this day, " +
+                "\n | although with smaller objects (since all the larger ones became the planets we know)." +
+                "\n | " +
+                "\n | Asteroids continue to come close to Earth in their orbits. " +
+                "\n | They are called near-Earth objects (NEOs) or Potentially Hazardous Asteroids (PHAs). " +
+                "\n | If orbital conditions are right, it’s possible that a NEO could crash to Earth’s surface. " +
+                "\n | Depending on the size of the impactor, the damage to our planet (and us) could range from very " +
+                "\n | little to catastrophic. A small incoming object would likely break up in our atmosphere. " +
+                "\n | " +
+                "\n | A larger one could rain very large pieces down on the surface or into the oceans. " +
+                "\n | There are groups of astronomers searching out and mapping the positions of these NEOs in " +
+                "\n | order to predict possible impacts weeks, months, or years in advance. " +
+                "\n | Several organizations are making plans in case something should hit and cause damage. " +
+                "\n | The chances of something hitting Earth are very small, but also depend on the size " +
+                "\n | and orbit of the object. \n");
+
+        System.out.println("---------------------------------------" +
+                "\n             Famous Asteroids" +
+                "\n---------------------------------------" +
+                "\n | The best-known asteroids are 1 Ceres, which is 952 kilometres across, " +
+                "\n | 2 Pallas (with a diameter of 544 kilometres), and 4 Vesta (roughly 580 km across). " +
+                "\n | " +
+                "\n | These are rocky minor planets, and astronomers have observed them since the 1800s. " +
+                "\n | Ceres is a differentiated asteroid. That means it has a rocky core and a icy outer crust. " +
+                "\n | It might have an internal ocean. Pallas has a very irregular shape, and may be what’s left of " +
+                "\n | an early protoplanet. " +
+                "\n | Vesta is very bright and is likely the leftover of a rocky protoplanet.");
+    }
+}
