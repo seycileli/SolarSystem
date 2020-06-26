d66ec7d1b863c943972d974a096331ffea2f6cbc
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-27 00:10:16 +0300

    Create TestingPrint.java

---
+++ test/TestingPrint.java
@@ -0,0 +1,17 @@
+package test;
+
+import entities.HeavenlyBody;
+import solarSystem.Planets;
+
+import java.util.*;
+
+public class TestingPrint {
+
+    private final static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private final static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+    //I used this Class to call methods from other Classes to print, view and change erros, typos & etc
+
+    }
+}
