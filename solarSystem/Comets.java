c54e855b38394dfdc55f2599456e12131b428c4f
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-06-27 00:03:42 +0300

    Create Comets.java

---
+++ solarSystem/Comets.java
@@ -0,0 +1,100 @@
+package solarSystem;
+
+public class Comets {
+
+    public static void generalInformation() {
+        System.out.println("\n---------------------------------------" +
+                "\n             About Comets ☄ " +
+                "\n---------------------------------------" +
+                "\n | A comet is a very small solar system body made mostly of ices mixed with smaller amounts of " +
+                "\n | dust and rock. Most comets are no larger than a few kilometres across. " +
+                "\n | The main body of the comet is called the nucleus, and it can contain water, methane, nitrogen and other ices." +
+                "\n | " +
+                "\n | When a comet is heated by the Sun, its ices begin to sublimate (similar to the way dry ice " +
+                "\n | “fizzes” when you leave it in sunlight). " +
+                "\n | The mixture of ice crystals and dust blows away from the comet nucleus in the solar wind, " +
+                "\n | creating a pair of tails. The dust tail is what we normally see when we " +
+                "\n | view comets from Earth." +
+                "\n | " +
+                "A plasma tail also forms when molecules of gas are “excited” by interaction with the solar wind. " +
+                "\n | The plasma tail is not normally seen with the naked eye, but can be imaged. Comets normally" +
+                "\n | orbit the Sun, and have their origins in the Oort Cloud and Kuiper Belt" +
+                "\n | regions of the outer solar system.");
+
+        System.out.println(
+                "\n ~ Facts about Comets ☄ " +
+                        "\n There are many misconceptions about comets, which are simply pieces of solar system ices " +
+                        "\n travelling in orbit around the Sun. Here are some fascinating and true facts about comets." +
+                        "\n \t (1) The nucleus of a comet is made of ice and can be as small as a few meters across " +
+                        "\n            to giant boulders a few kilometres across." +
+                        "\n \t (2) The closest point in a comet’s orbit to the Sun is called “perihelion”. " +
+                        "\n            The most distant point is called “aphelion”." +
+                        "\n \t (3) As a comet gets closer to the Sun, it begins to experience heat. That causes some " +
+                        "\n            of its ices to sublimate (similar to dry ice sizzling in sunlight). " +
+                        "\n            If the ice is close to the comet’s surface, it may form a small “jet” of " +
+                        "\n            material spewing out from the comet like a mini-geyser." +
+                        "\n \t (4) Material streams from comets and populates the comet’s orbit. " +
+                        "\n            If Earth (or another planet) happens to move through that stream, " +
+                        "\n            those particles fall to Earth as meteor showers." +
+                        "\n \t (5) As a comet gets close to the Sun, it loses some of its mass due to the sublimation. " +
+                        "\n            If a comet goes around enough times, it will eventually break up. " +
+                        "\n            Comets also break up if they come TOO close to the Sun or another " +
+                        "\n            planet in their orbits." +
+                        "\n \t (6) Comets are usually made of frozen water and supercold methane, " +
+                        "\n            ammonia and carbon dioxide ices. Those are mixed with rock, dust, " +
+                        "\n            and other metallic bits of solar system debris." +
+                        "\n \t (7) Comets have two tails: a dust tail (which you can see with the naked eye)" +
+                        "\n            and a plasma tail, which is easily photographed but difficult to see with your eyes." +
+                        "\n \t (8) Comet orbits are usually elliptical." +
+                        "\n \t (9) Many comets formed in the Oort Cloud and Kuiper Belts, " +
+                        "\n            two of the outermost regions of the solar system." +
+                        "\n \t (10) Comets are not spaceships or alien bases. " +
+                        "\n            They are fascinating bits of solar system material that date back " +
+                        "\n            to the formation of the Sun and planets. \n");
+
+        System.out.println("---------------------------------------" +
+                "\n             Comet Naming ☄" +
+                "\n---------------------------------------" +
+                "\n | Comets come in several categories. The most common are periodic and non-periodic." +
+                "\n | " +
+                "\n | In the past, comets were named for their discoverers, such as Comet Halley for Sir Edmond Halley. " +
+                "\n | In modern times, comet names are governed by rules set forth by the International " +
+                "\n | Astronomical Union (IAU). " +
+                "\n | A comet is given an official designation, and can also be identified by the last names of " +
+                "\n | up to three independent discoverers." +
+                "\n | " +
+                "\n | Here’s how it works. Once a comet has been confirmed, the following naming rules are followed." +
+                "\n | First, if the comet is a periodic comet, then it is indicated with a P/ followed by the year" +
+                "\n |  of its discovery, a letter indicating the half-month in which it was discovered, " +
+                "\n | followed by a number indicating its order of discovery. " +
+                "\n | So, for example, the second periodic comet found in the first half of January, 2015 " +
+                "\n | would be called P/2015 A2." +
+                "\n | " +
+                "\n | A non-periodic comet would be indicated with a C/ followed by the year of its discovery, " +
+                "\n | a letter indicating the half-month in which it was discovered, " +
+                "\n | followed by a number indicating its order of discovery." +
+                "\n | " +
+                "\n | If a comet is independently discovered by three people named Smith, Jones, and Petersen, " +
+                "\n | it could also be called Comet Smith-Jones-Petersen, in addition to its formal designation. " +
+                "\n | Today, many comets are found through automated instrument searches, and so the formal " +
+                "\n | designations are more commonly used. \n");
+
+        System.out.println("---------------------------------------" +
+                "\n             Famous Comets ☄" +
+                "\n---------------------------------------" +
+                "\n | Well-known comets include the non-periodic comets Hale-Bopp (C/1995 O1), " +
+                "\n | Hyakutake (C/1996 B2), McNaught (C2006 P1), and Lovejoy (C/2011 W3). These flared brightly in " +
+                "\n | our skies and then faded into obscurity." +
+                "\n | " +
+                "\n | In addition, Comet Shoemaker-Levy 9 (D/1993 F2) was spotted after it had broken up after a close " +
+                "\n | call with Jupiter. " +
+                "\n | (The D in its proper designation means it has disappeared or is determined to no longer exist). " +
+                "\n | More than a year later, the pieces of the comet crashed into Jupiter." +
+                "\n | " +
+                "\n | The periodic Comet Halley (1P/Halley) is the most famous in history. " +
+                "\n | It returns to the inner solar system once every 76 years. " +
+                "\n | Other well-known periodic comets include 2P/Encke, which appears ever 3.3 years " +
+                "\n | and 9P/Tempel (Tempel 2), which was visited by the Deep Impact and Stardust probes, " +
+                "\n | and makes perihelion around the Sun every 5.5 years.");
+    }
+}
